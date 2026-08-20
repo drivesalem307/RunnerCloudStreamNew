131ffc52fa2a942ca19b07d39a0be1068a4b7b8f
drivesalem307 <drivexodus93@gmail.com>
2026-08-20 17:33:02 +0300

    Update Asia2TVPlugin.kt

--- Asia2TV/src/main/kotlin/com/asia2tv/Asia2TVPlugin.kt
+++ Asia2TV/src/main/kotlin/com/asia2tv/Asia2TVPlugin.kt
@@ -1,19 +1,21 @@
 package com.asia2tv
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import com.lagradost.cloudstream3.utils.AppUtils.openWebView
 
 @CloudstreamPlugin
 class Asia2TVPlugin : Plugin() {
     override fun load(context: Context) {
-        // تسجيل المزود الرئيسي
         registerMainAPI(Asia2TV())
 
-        // إضافة زر الإعدادات المخصص المماثل لمستودع Phisher
         openSettings = { ctx ->
-            ctx.openWebView("https://asia2tv.com/login", "تسجيل الدخول - Asia2TV")
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://asia2tv.com/login")).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+            ctx.startActivity(intent)
         }
     }
 }
