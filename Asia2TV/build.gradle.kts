281d565b2029b021ecc0a2e42e104e19d944ad19
drivesalem307 <drivexodus93@gmail.com>
2026-08-20 14:01:11 +0300

    Create build.gradle.kts

---
+++ Asia2TV/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+android {
+    compileSdk = 35
+    defaultConfig {
+        minSdk = 21
+    }
+}
