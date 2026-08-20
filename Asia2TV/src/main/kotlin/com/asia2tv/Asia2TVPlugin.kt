19be17e037454de130bd20f2802f48ff30d80de3
drivesalem307 <drivexodus93@gmail.com>
2026-08-20 17:49:11 +0300

    Update Asia2TVPlugin.kt

--- Asia2TV/src/main/kotlin/com/asia2tv/Asia2TVPlugin.kt
+++ Asia2TV/src/main/kotlin/com/asia2tv/Asia2TVPlugin.kt
@@ -1,8 +1,6 @@
 package com.asia2tv
 
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
@@ -10,12 +8,5 @@ import com.lagradost.cloudstream3.plugins.Plugin
 class Asia2TVPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(Asia2TV())
-
-        openSettings = { ctx ->
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://asia2tv.com/login")).apply {
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            }
-            ctx.startActivity(intent)
-        }
     }
 }
